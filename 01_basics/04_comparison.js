bba58e4011d05f81a21415dafd368c82378c9a39
komal suthar <110638826+sutharkomal16@users.noreply.github.com>
2024-01-08 19:35:29 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);  
+// console.log(2 >= 1); 
+// console.log(2 < 1);  
+// console.log(2 == 1); 
+// console.log(2 != 1); 
+
+
+// console.log("2" > 1);   
+// console.log("02" > 1);  
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===  IT checks not only string as no but also its data type ["2" == 2 => T, "2" === 2 => F]
+
+// console.log("2" === 2);
