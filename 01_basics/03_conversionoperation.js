5bfcbbd34b7486d418c27d177f03ba85f807a0e1
komal suthar <110638826+sutharkomal16@users.noreply.github.com>
2024-01-08 18:27:57 +0000

    operations, prefix and postfix

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -27,7 +27,49 @@
     let number2 = 456.567;
     let valueInString = String(number2);
 
-    console.log(number2)
-    console.log(typeof valueInString);
+    // console.log(number2)
+    // console.log(typeof valueInString);
 
-    
+    //  ************************************************** OPERATIONS ********************************************************
+
+    let no1 = 45;
+    let no2 = 5;
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+    let str1 = "Komal";
+    let str2 = "Suthar";
+
+    let strTotal = str1 + str2;
+
+    // console.log(strTotal);
+
+    // console.log("1" + 2);
+    // console.log(1 + "8");
+    // console.log("1" + 2 + 2);
+    // console.log(1 + 2 + "2");
+    // console.log( (3 + 4) * 5 % 3);
+
+    // console.log(+true)
+    // console.log(+"")
+
+    // ************************* PREFIX   **************************
+    let a = 12;
+    let b = ++a ;
+
+    // console.log(a)
+    // console.log(b)
+    // console.log(`a:${a}, b:${b}`);
+
+     // ************************* POSTFIX   **************************
+     let x = 12;
+     let y = x++ ;
+ 
+     console.log(x)
+     console.log(y)
+     console.log(`x:${x}, y:${y}`);
