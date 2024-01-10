8ce75fb99183928c3c4227f48eb1737f15de65fb
komal suthar <110638826+sutharkomal16@users.noreply.github.com>
2024-01-10 06:59:06 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,22 @@
+const gameName = "komal";
+let surname = "Suthar";
+let trimEg = "    komal   "
+// console.log(gameName.toUpperCase());
+// console.log(`this is ${gameName} and this is ${matchName}`)
+// console.log(matchName.toUpperCase())
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('m'));
+// console.log(gameName.substring(0,3)); 
+// * you cannot use negative value in substring like (-8,4). It will default take 0 instead of (-8)
+
+// console.log(surname.slice(1,3));
+console.log(trimEg.trim());
+// * trim() => it is use to trim whitespace aroung word
+
+let url = "https://symmetrical-fortnight-wp5jvqx6r47399w5.github.dev/";
+console.log(url.replace('-',"komal"))
+// https://symmetricalkomalfortnight-wp5jvqx6r47399w5.github.dev/
+
+let name1 = "komal-nisha-khushi-antima";
+console.log(name1.split('-'))
+// => [ 'komal', 'nisha', 'khushi', 'antima' ] 
