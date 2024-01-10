ebb8702b8c797b514231dd6fbc8c7cdddb6e2b97
komal suthar <110638826+sutharkomal16@users.noreply.github.com>
2024-01-10 07:00:06 +0000

    data type summary

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -1,6 +1,6 @@
 // / Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt  => STACK
 
 const score = 100
 const scoreValue = 100.3
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 
 
@@ -20,7 +20,7 @@ console.log(id === anotherId);
 
 // Reference (Non primitive)
 
-// Array, Objects, Functions
+// Array, Objects, Functions           => HEAP
 
 const heros = ["shaktiman", "spiderman", "captain america"];
 let myObj = {
@@ -32,6 +32,26 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ************ STACK [PRIMITIVE DATA TYPE => its original value stay as it is bcz its copy value is assign to variable ] 
+// ************ HEAP [NON-PRIMITIVE DATA TYPE => its original value change when its assign variable value is change bcz it refer to direct address reference]
+
+let myName = "KOMAL";
+let username = myName;
+myName = "NISHA"
+
+// console.log(myName);
+// console.log(username);
+
+let user1 = {
+    userEmail:"komal@gmail.com"
+}
+
+let user2 = user1;
+user2.userEmail= "nisha@gmail.com"
+
+// console.log(user1.userEmail);
+// console.log(user2.userEmail);
